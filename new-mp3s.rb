4988eb59d3c5d97711cdc323565ba8846e30a5d8
Jason Zhao <jasonzhao6@gmail.com>
2015-12-01 05:18:44 -0800

    Update script to reflect new design

--- new-mp3s.rb
+++ new-mp3s.rb
@@ -1,46 +1,45 @@
-# MP3s from latest episodes are not behind a paywall
+# MP3s from latest interviews are not behind a paywall
 # Run this script every couple of days to download them
 
 # HOWTO:
-# - if scraping all latest episodes, reset `LAST_SCRAPED_ID` to nil
+# - if scraping all latest interviews, reset `LAST_SCRAPED_URL` to nil
 # - run script, copy printed URLs into a download manager like JDownloader
-# - follow instruction to set `LAST_SCRAPED_ID`
+# - follow instruction to set `LAST_SCRAPED_URL`
 
 require 'nokogiri'
 require 'open-uri'
 
-LAST_SCRAPED_ID = 1226
+LAST_SCRAPED_URL = 'http://mixergy.com/interviews/ian-crosby-bench-accounting/'
 
-def latest_episodes
+def latest_interview_urls
   url = 'http://mixergy.com/interviews/'
   page = Nokogiri::HTML(open(url))
-  rows = page.css('#interview-archive-table tr')
+  interviews = page.css('.card.interview')
+  urls = []
 
-  # First row is the heading
-  # The rest are episodes
-  rows[1..-1].map do |row|
-    id = row.css('td:first').first.text.to_i
-    next if LAST_SCRAPED_ID >= id
-    url = row.css('a')[0]['href']
-    { id: id, url: url }
-  end.compact
+  interviews.each do |row|
+    url = row.css('.content a')[0]['href']
+    break if LAST_SCRAPED_URL == url
+    urls << url
+  end
+
+  urls
 end
 
-def print_mp3_url(episode)
-  url = episode[:url]
-  page = Nokogiri::HTML(open(url))
+def print_mp3_url(interview_url)
+  page = Nokogiri::HTML(open(interview_url))
   puts page.css('source').first['src']
 end
 
-episodes = latest_episodes
-episodes.each do |episode|
-  print_mp3_url(episode) rescue break
+urls = latest_interview_urls
+urls.each do |interview_url|
+  print_mp3_url(interview_url) rescue break
   sleep 0.5
 end
 
-# Print instruction to reset `LAST_SCRAPED_ID`
-if !episodes.empty? && LAST_SCRAPED_ID != episodes[0][:id]
+# Print instruction to reset `LAST_SCRAPED_URL`
+if !urls.empty? && LAST_SCRAPED_URL != urls[0]
   puts
-  puts 'TODO:'
-  puts "set LAST_SCRAPED_ID = #{ episodes[0][:id] }"
+  puts 'TODO: set'
+  puts "LAST_SCRAPED_URL = '#{ urls[0] }'"
 end
