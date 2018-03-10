78d59f3b7296301d87933f168d49c56cce1bb960
Jason Zhao <jasonzhao6@gmail.com>
2018-03-10 14:27:28 -0800

    One mp3 url was redirecting unexpectedly; add check to short circuit

--- new-mp3s.rb
+++ new-mp3s.rb
@@ -1,16 +1,16 @@
-# MP3s from latest interviews are not behind a paywall
-# Run this script every couple of days to download them
+# MP3s from new episodes are downloadable
+# They'll be behind a paywall in about a week
 
 # HOWTO:
-# - if scraping all latest interviews, reset `LAST_SCRAPED_URL` to nil
-# - run script, copy printed URLs into a download manager like JDownloader
-# - follow instruction to set `LAST_SCRAPED_URL`
+# - set `LAST_SCRAPED_URL` to nil to download all episodes not yet behind a paywall
+# - run script weekly, copy printed URLs into a download manager like JDownloader
+# - follow printed TODO and update `LAST_SCRAPED_URL` in this file
 
 require 'nokogiri'
 require 'open-uri'
 require 'json'
 
-LAST_SCRAPED_URL = 'https://mixergy.com/interviews/freeeup-with-nathan-hirsch/'
+LAST_SCRAPED_URL = 'https://mixergy.com/interviews/with-sachit-gupta/'
 
 def latest_interview_urls
   url = 'https://mixergy.com/interviews/'
@@ -29,6 +29,8 @@ end
 
 def print_mp3_url(interview_url)
   mp3_line = open(interview_url).select{ |source_code| source_code =~ /mp3/ }.first
+  return unless mp3_line
+
   json_start = mp3_line.index('{')
   json_end = mp3_line.rindex('}')
   json_object = JSON.parse(mp3_line[json_start..json_end])
@@ -44,6 +46,6 @@ end
 # Print instruction to reset `LAST_SCRAPED_URL`
 if !urls.empty? && LAST_SCRAPED_URL != urls[0]
   puts
-  puts 'TODO: set'
+  puts 'TODO: update'
   puts "LAST_SCRAPED_URL = '#{ urls[0] }'"
 end
